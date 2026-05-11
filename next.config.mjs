765dbf99b3c9e3b1bc3c8b43cb078b7ee95350cf
nomis <simon@cogito.dk>
2026-05-11 23:32:58 +0200

    chore: sync 2026-05-11 23:32

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/https-simo899t.github.io-typstgame',
+  assetPrefix: '/https-simo899t.github.io-typstgame',
   typescript: {
     ignoreBuildErrors: true,
   },
